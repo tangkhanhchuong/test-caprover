49c9ba4759aac99f10ccb846e6d75982b7808a70
tkc <chuongtangkhanh2104@gmail.com>
2022-04-24 10:53:54 +0700

    feat: add route

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
   res.json('hello');
 })
 
+app.get('/user', (req, res) => {
+  res.json([
+    {
+      name: "tkc",
+      age: 22
+    },
+    {
+      name: "tkh",
+      age: 18
+    },
+    {
+      name: "lnc",
+      age: 51
+    },
+    {
+      name: "tks",
+      age: 56
+    }
+  ]);
+})
+
 app.listen(process.env.PORT || 8000, () => {
   console.log('listen');
 })
